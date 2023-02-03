35cc105b359315e4f5f2ef273788e33c18dfacd9
TheSurajMore <surajmbusiness@gmail.com>
2023-02-03 20:59:13 +0530

    LandingPage

--- my_ecommerce/src/Components/Products.jsx
+++ my_ecommerce/src/Components/Products.jsx
@@ -1,8 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {Grid, GridItem, extendTheme} from '@chakra-ui/react'
+import ProductCard from "./ProductCard";
 
 const Products = () => {
 
+    const [data, setData] = useState([]);
+
+    const getData = () =>{
+  axios.get('https://fakestoreapi.com/products')
+  .then(function (response) {
+    // handle success
+    console.log(response.data);
+    setData(response.data);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+}
+
+    useEffect(()=>{
+        getData()
+    },[])
+
+    const breakpoints = {
+      sm: '320px',
+      md: '768px',
+      lg: '960px',
+      xl: '1200px',
+      '2xl': '1536px',
+    }
+
+    const theme = extendTheme({ breakpoints })
+
     return(<>
+            {/* <Grid templateColumns='repeat(4, 1fr)' gap={6}> */}
+            <Grid templateColumns={{ base:'repeat(1, 1fr)', sm:'repeat(1, 1fr)', md: 'repeat(2, 1fr)', lg:'repeat(3, 1fr)', xl:'repeat(4, 1fr)' }} gap={6}>
+    {data && data.map((el)=>(
+        <GridItem w='100%' key={el.id} >
+            <ProductCard IMAGE={el.image} title={el.title} price={el.price} description={el.description} category={el.category}
+             rate={el.rating.rate} count={el.rating.count} />
+        </GridItem>
+    ))}
+          </Grid>
     </>)
 }
 
