426f1754ddb8d4ac538f3607e1142102929e4c0d
TheSurajMore <surajmbusiness@gmail.com>
2023-02-20 08:28:08 +0530

    20/02/2023

--- my_ecommerce/src/Components/BuyNow.jsx
+++ my_ecommerce/src/Components/BuyNow.jsx
@@ -2,13 +2,20 @@ import React, { useContext } from "react";
 import { Navigate,} from "react-router-dom";
 import { AuthenticationContext } from "../Contexts/Authentication";
 import Address from "./Address";
+import {Box, Select} from '@chakra-ui/react'
 
 const BuyNow = () => {
     const {Auth} = useContext(AuthenticationContext);
 
     return(<>
         { Auth===false?<Navigate to='/' replace />:
+        <>
     <Address/>
+    <Select placeholder='Select option' width={'30%'} >
+    <option value='option1'>Option 1</option>
+    <option value='option2'>Option 2</option>
+    </Select>
+        </>
 }
     </>)
 }
